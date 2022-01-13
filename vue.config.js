25fcbfed17783dca3184d6cb6b5a904269362a5a
DESKTOP-I47NBJ4\seeroo <gahee.oh@seeroo.co.kr>
2022-01-13 17:16:10 +0900

    css 수정

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     proxy: {
       "/be": {
         //프록시요청을 보낼 주소.
-        target: "http://localhost:25123", //test 서버
+        target: "http://192.168.0.130:25123", //test 서버
         changeOrigin: true,
         ws: false,
       },
